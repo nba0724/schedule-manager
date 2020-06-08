35d76d8bc555838db0cb12a2d2295792db760aa7
AGX Nishiyama <nishiyama.y@one-all.co.jp>
2020-06-08 15:16:52 +0900

    LINE SDK 登録

--- server/routers/hook-router.js
+++ server/routers/hook-router.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const line = require("@line/bot-sdk");
 
+const lineConfig = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_SECRET_KEY
+};
+
+router.use(line.middleware(lineConfig));
 router.post("/", (req, res) => {
   return res.json({ test: "hook" });
 });
